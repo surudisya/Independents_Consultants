787399f6ab91aff70bbee57f0a717d3832505805
Dhanushan <dhanushan.info@gmail.com>
2024-11-20 20:52:37 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     <Router>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/service" element={<Service />} />
+            {/* <Route path="/service" element={<Service />} /> */}
 
 
         </Routes>
